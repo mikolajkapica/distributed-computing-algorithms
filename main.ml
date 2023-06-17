19c7fb674e6839a7b80ab54ecb3f1064ca2e4b7b
Mikolaj Kapica <mikolajkapica@mail.com>
2023-06-17 16:52:42 +0200

    lazy init

--- main.ml
+++ main.ml
@@ -1,11 +1,12 @@
 type process = { 
     process_id : string; 
-    power : float; 
+    power : int; 
     time : int }
 
 type computer = {
   computer_id : string;
-  mutable usage : float;
+  mutable usage : int;
+  mutable current_processes : process list;
   mutable processes : process list;
 }
 
@@ -14,54 +15,113 @@ let create_computers n =
   let create_processes n =
     let rec aux acc = function
       | 0 -> acc
-      | n ->
-          aux
-            ({
-               process_id = string_of_int n;
-               power = Random.float 100.;
-               time = Random.int 10;
-             }
-            :: acc)
-            (n - 1)
+      | n -> match Random.bool () with
+          | false -> aux ({process_id = "-1"; power = 0; time = 0} :: acc) (n - 1)
+          | true ->
+              aux
+                ({
+                   process_id = string_of_int n;
+                   power = Random.int 100;
+                   time = Random.int 10;
+                 }
+                :: acc)
+                (n - 1)
     in
     aux [] n
   in
   let rec aux acc = function
     | 0 -> acc
     | n ->
-        aux
-          ({ computer_id = string_of_int n; usage = 0.; processes = create_processes 4 } :: acc)
+          aux
+          ({
+             computer_id = string_of_int n;
+             usage = 0;
+             current_processes = [];
+             processes = create_processes (Random.int 100);
+            }
+          :: acc)
           (n - 1)
-  in
-  aux [] n
+    in
+    aux [] n
 
 (* iterate over the list of computers and print the state of each one and the list of processes *)
 let print_computers computers =
   let print_process processes =
-    Printf.printf "{Process: %s: power: %f}, " processes.process_id
-      processes.power
+    Printf.printf "{Process: %s: power: %i}, " processes.process_id processes.power
   in
   List.iter
     (fun computer ->
-      Printf.printf "Computer: %s: usage: %f " computer.computer_id
-        computer.usage;
+      Printf.printf "Computer: %s: usage: %i " computer.computer_id computer.usage;
       List.iter (fun process -> print_process process) computer.processes;
-      Printf.printf "\n")
+      Printf.printf "\n\n")
     computers
 
-(* create processes for a computer *)
+(* iterate over the list of computers
+   get first process in the list of processes 
+   if the process has id -1, then remove it
+   else:
+       ask z random computers to execute the process
+           if they agree, then add it to the list of their current processes
+           else, take it if we can
+   update the usage of computer*)
 
-let add_processes_to_computer computer processes =
-  let add_process_to_computer computer process =
-    computer.processes <- process :: computer.processes;
-    computer.usage <- computer.usage +. process.power
-  in
-  List.iter (fun process -> add_process_to_computer computer process) processes
+    (* let computers = create_computers n in *)
+    (* let rec time_tick computers = *)
+    (*     match computers with *)
+    (*     | [] -> () *)
+    (*     | computer :: rest -> *)
+    (*         let process = List.hd computer.processes in *)
+    (*         match process.process_id with *)
+    (*         | "-1" ->  *)
+    (*                 computer.processes <- List.tl computer.processes;  *)
+    (*                 time_tick rest *)
+    (*         | _ -> *)
+    (*             let rec ask not_asked_computers count = function *)
+    (*                 | 0 -> [] *)
+    (*                 | z ->  *)
+    (*                     let random_computer = List.nth not_asked_computers (Random.int (List.length not_asked_computers)) in *)
+    (*                     let not_asked_computers = List.filter (fun x -> x.computer_id <> random_computer.computer_id) *)
+    (*                     if random_computer.usage + process.power <= 100 then *)
+    (*                         random_computer.current_processes <- random_computer.current_processes @ [process]; *)
+    (*                         random_computer.usage <- random_computer.usage +. process.power; *)
+    (*                     random_computer :: ask not_asked_computers (count + 1) *)
+    (*  *)
+    (*             let not_asked_computers = computers in *)
+    (*             let random_computers = ask not_asked_computers z in *)
+    (*             computer.processes <- List.tl computer.processes; *)
+    (*             time_tick (rest @ random_computers) *)
+
+let simulate_lazy p r z n = 
+    let computers = create_computers n in
+    let rec time_tick time_left computers =
+        match time_left with
+        | 0 -> ()
+        | _ ->
+            let rec handle_computers computers =
+                match computers with
+                | [] -> ()
+                | computer :: rest ->
+                    (* handle new process *)
+                    let process = List.hd computer.processes in
+                    let get_accepting_computer = match process.process_id with
+                    | "-1" -> 
+                            computer.processes <- List.tl computer.processes; 
+                            None
+                    | _ ->
+                        let accepting_computer = let rec ask not_asked_computers count =
+                          match count with
+                          | 0 -> []
+                          | z ->
+                            let random_computer = List.nth not_asked_computers (Random.int (List.length not_asked_computers)) in
+                            if random_computer.usage + process.power <= 100 then 
+                                random_computer.current_processes <- random_computer.current_processes @ [process];
+                                random_computer.usage <- random_computer.usage + process.power;
+                                random_computer :: ask not_asked_computers z;
 
 let () =
-  let p = 0.5 in
-  let z = 3 in
-  let r = 0.2 in
-  let n = 4 in
-  let computers = create_computers n in
-  print_computers computers
+    let p = 0.5 in
+    let r = 0.2 in
+    let z = 3 in
+    let n = 10 in
+    simulate_lazy p r z n 
+
